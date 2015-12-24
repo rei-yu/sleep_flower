6e8201b81e600000f0c35c7eefbfe3f93e098a1e
rei___yu <satomi.i.sakura@gmail.com>
2015-12-24 17:53:12 +0900

    fix bug and adapt team

--- public/javascript/get_data.js
+++ public/javascript/get_data.js
@@ -13,37 +13,83 @@ function showInfo(data, tabletop) {
 
 function parseData(data) {
   var id = store.get("user_id");
+  console.log(id);
+  if (id == 1 || id == 2 || id == 3) {
+    score_culc(1, data, true);
+  } else {
+    score_culc(id, data, false);
+  }
+}
+
+function score_culc (id, data, is_team) {
   var score = 100;
   var get_leaf_count = 0;
   var lose_leaf_count = 0;
   var has_leaf = false;
 
-  for (i=0; i<data.length; i++) {
-    if (data[i]["User ID"] == id) {
-      console.log("id", data[i]["User ID"]);
-      console.log("yellow", data[i]["｢少し眠そう｣の回数(黄色)"]);
-      console.log("orange", data[i]["｢とても眠そう｣の回数(オレンジ色)"]);
+  if (is_team) {
+    score = 300;
+    for (i=0; i<data.length; i++) {
+      if (data[i]["User ID"] == 1 || data[i]["User ID"] == 2 || data[i]["User ID"] == 3) {
+        // console.log("id", data[i]["User ID"]);
+        // console.log("yellow", data[i]["｢少し眠そう｣の回数(黄色)"]);
+        // console.log("orange", data[i]["｢とても眠そう｣の回数(オレンジ色)"]);
+        console.log("score: ", score);
 
-      var yellow = data[i]["｢少し眠そう｣の回数(黄色)"];
-      var orange = data[i]["｢とても眠そう｣の回数(オレンジ色)"];
+        var yellow = data[i]["｢少し眠そう｣の回数(黄色)"];
+        var orange = data[i]["｢とても眠そう｣の回数(オレンジ色)"];
 
-      score -= (yellow * 10 + orange * 20);
-      if (score >= 80) {
-        get_leaf_count += 1;
-        lose_leaf_count = 0;
-      } else if (score < 40) {
-        get_leaf_count = 0;
-        lose_leaf_count += 1;
-      } else {
-        get_leaf_count = 0;
-        lose_leaf_count = 0;
+        if (yellow == 0 && orange == 0) {
+          score += 15
+        } else {
+          score -= (yellow * 10 + orange * 20);
+        }
+
+        if (score >= 80) {
+          get_leaf_count += 1;
+          lose_leaf_count = 0;
+        } else if (score < 40) {
+          get_leaf_count = 0;
+          lose_leaf_count += 1;
+        } else {
+          get_leaf_count = 0;
+          lose_leaf_count = 0;
+        }
+      }
+    }
+    score = score / 3;
+  } else {
+    for (i=0; i<data.length; i++) {
+      if (data[i]["User ID"] == id) {
+        console.log("score: ", score);
+
+        var yellow = data[i]["｢少し眠そう｣の回数(黄色)"];
+        var orange = data[i]["｢とても眠そう｣の回数(オレンジ色)"];
+
+        if (yellow == 0 && orange == 0) {
+          score += 15
+        } else {
+          score -= (yellow * 10 + orange * 20);
+        }
+
+        if (score >= 80) {
+          get_leaf_count += 1;
+          lose_leaf_count = 0;
+        } else if (score < 40) {
+          get_leaf_count = 0;
+          lose_leaf_count += 1;
+        } else {
+          get_leaf_count = 0;
+          lose_leaf_count = 0;
+        }
       }
     }
   }
-
   if (get_leaf_count >2 ) {
     has_leaf = true;
   }
+
+  console.log("score", score);
 
   get_flower(score, has_leaf);
 }
